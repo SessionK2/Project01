4b5520393046018feaade33fa7049b96eb72613d
zhangsen <ws_zhangsen@163.com>
2020-04-23 17:24:18 +0800

    2020-4-23 17:24:08 提交了

--- Module01/src/com/zs/test/Customer.java
+++ Module01/src/com/zs/test/Customer.java
@@ -1,16 +1,13 @@
 package com.zs.test;
 
+import java.util.Date;
+
 public class Customer {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.println("i="+i);
         }
-        if (args != null) {
-            //2020年4月23日16:45:34
-        }
-        if (args == null) {
-            //2020-4-23 17:07:12
-        }
+        System.out.println("时间："+ new Date());
 
 
 //        System.out.println("hello zhouaiqin!");
